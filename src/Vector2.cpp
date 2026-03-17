21ce02e3c6bd56137ecce6abf2bf16d5b04146dd
zahra <iqbalzahra016@gmail.com>
2026-03-17 15:38:59 +0000

    Update Vector2.cpp

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -1,1 +1,27 @@
+#include "Vector2.h"
 
+Vector2 Vector2::operator+(const Vector2& other) const {
+    return Vector2(x + other.x, y + other.y);
+}
+
+Vector2 Vector2::operator-(const Vector2& other) const {
+    return Vector2(x - other.x, y - other.y);
+}
+
+Vector2 Vector2::operator*(double scalar) const {
+    return Vector2(x * scalar, y * scalar);
+}
+
+Vector2 Vector2::operator/(double scalar) const {
+    return Vector2(x / scalar, y / scalar);
+}
+
+double Vector2::magnitude() const {
+    return std::sqrt(x * x + y * y);
+}
+
+Vector2 Vector2::normalize() const {
+    double mag = magnitude();
+    if (mag == 0) return Vector2(0, 0);
+    return (*this) / mag;
+}
