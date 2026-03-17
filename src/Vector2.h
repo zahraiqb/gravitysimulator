346e63476cd3870b1f6e770d8fc83b4cc794cbb8
zahra <iqbalzahra016@gmail.com>
2026-03-17 15:35:52 +0000

    Update Vector2.h

--- src/Vector2.h
+++ src/Vector2.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <cmath>
 
+struct Vector2 {
+    double x;
+    double y;
+
+    Vector2() : x(0), y(0) {}
+    Vector2(double x, double y) : x(x), y(y);
+
+    Vector2 operator+(const Vector2& other) const;
+    Vector2 operator-(const Vector2& other) const;
+    Vector2 operator*(double scalar) const;
+    Vector2 operator/(double scalar) const;
+
+    double magnitude() const;
+    Vector2 normalize() const;
+};
